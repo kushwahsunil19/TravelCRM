ccd0c792537f23e6a39fee2f84dba670c2d795b5
Pranavvishwakarma <pranav213234@gmail.com>
2024-10-18 11:43:51 +0530

    update

--- resources/views/admin/quotations/index.blade.php
+++ resources/views/admin/quotations/index.blade.php
@@ -1,11 +1,8 @@
 @extends('admin.layouts.master')
 @section('content')
-
-
 <!-- Main Wrapper -->
 @include('admin.layouts.common-sidebar')
 <!-- /Main Wrapper -->
-
 <!-- Page Wrapper -->
 <div class="page-wrapper">
     <div class="content container-fluid">
