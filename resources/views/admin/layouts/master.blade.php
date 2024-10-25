2802cef91b5827b929b616ebf695113cd5b08dea
unknown <kushwahsunil19@gmail.com>
2024-10-25 15:12:56 +0530

    update

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -1,90 +1,106 @@
 <!DOCTYPE html>
-<html lang="en" data-layout="vertical" data-topbar="light" data-sidebar="light"  data-sidebar-size="lg" data-sidebar-image="none">
-	
+<html lang="en" data-layout="vertical" data-topbar="light" data-sidebar="light" data-sidebar-size="lg"
+    data-sidebar-image="none">
+
 <!-- Mirrored from kanakku.dreamstechnologies.com/html/template/users.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 07 Feb 2024 11:49:49 GMT -->
+
 <head>
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<!-- <title>@yield('title')</title> -->
-		<title>CENTURION LUXURY TRAVELS | UAE</title>
-		
-		<!-- Favicon -->
-		<link rel="shortcut icon" type="image/x-icon" href="{{url('/public/assets/img/favicon.png')}}">
-		
-		<!-- Bootstrap CSS -->
-		<link rel="stylesheet" href="{{url('/public/assets/css/bootstrap.min.css')}}">
-        
-        <!-- Font family -->
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <title>@yield('title')</title> -->
+    <title>CENTURION LUXURY TRAVELS | UAE</title>
 
-		<!-- Fontawesome CSS -->
-		<link rel="stylesheet" href="{{url('/public/assets/plugins/fontawesome/css/fontawesome.min.css')}}">
-		<link rel="stylesheet" href="{{url('/public/assets/plugins/fontawesome/css/all.min.css')}}">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{url('/public/assets/img/favicon.png')}}">
 
-		<!-- Feather CSS -->
-		<link rel="stylesheet" href="{{url('/public/assets/plugins/feather/feather.css')}}">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{url('/public/assets/css/bootstrap.min.css')}}">
 
-		<!-- Datepicker CSS -->
-		<link rel="stylesheet" href="{{url('/public/assets/css/bootstrap-datetimepicker.min.css')}}">
+    <!-- Font family -->
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
+        rel="stylesheet">
 
-		<!-- Select2 CSS -->
-		<link rel="stylesheet" href="{{url('/public/assets/plugins/select2/css/select2.min.css')}}">
-		
-		<!-- Datatables CSS -->
-		<link rel="stylesheet" href="{{url('/public/assets/plugins/datatables/datatables.min.css')}}">
-		
-		<!-- Main CSS -->
-		<link rel="stylesheet" href="{{url('/public/assets/css/style.css')}}">
-		<link rel="stylesheet" href="{{url('/public/assets/css/custom-style.css')}}">
+    <!-- Fontawesome CSS -->
+    <link rel="stylesheet" href="{{url('/public/assets/plugins/fontawesome/css/fontawesome.min.css')}}">
+    <link rel="stylesheet" href="{{url('/public/assets/plugins/fontawesome/css/all.min.css')}}">
 
-		<!-- Layout Js -->
-		<script src="{{url('/public/assets/js/layout.js')}}"></script>
-		<!-- Summernote CSS -->
-        <link rel="stylesheet" href="{{url('/public/assets/plugins/summernote/summernote-bs4.min.css')}}">
-      
-	</head>
-	<body>
+    <!-- Feather CSS -->
+    <link rel="stylesheet" href="{{url('/public/assets/plugins/feather/feather.css')}}">
+
+    <!-- Datepicker CSS -->
+    <link rel="stylesheet" href="{{url('/public/assets/css/bootstrap-datetimepicker.min.css')}}">
+
+    <!-- Select2 CSS -->
+    <link rel="stylesheet" href="{{url('/public/assets/plugins/select2/css/select2.min.css')}}">
+
+    <!-- Datatables CSS -->
+    <link rel="stylesheet" href="{{url('/public/assets/plugins/datatables/datatables.min.css')}}">
+
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="{{url('/public/assets/css/style.css')}}">
+    <link rel="stylesheet" href="{{url('/public/assets/css/custom-style.css')}}">
+
+    <!-- Layout Js -->
+    <script src="{{url('/public/assets/js/layout.js')}}"></script>
+    <!-- Summernote CSS -->
+    <link rel="stylesheet" href="{{url('/public/assets/plugins/summernote/summernote-bs4.min.css')}}">
+
+</head>
+
+<body>
 
 
 
     @yield('content')
 
-  		<!-- /Theme Setting -->		
-		<!-- jQuery -->
-		<script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
-		<script src="{{url('/public/assets/js/jquery-3.7.1.min.js')}}"></script>
-		
-		<!-- Bootstrap Core JS -->
-		<script src="{{url('/public/assets/js/bootstrap.bundle.min.js')}}"></script>
+    <!-- /Theme Setting -->
+    <!-- jQuery -->
+    <script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+    <script src="{{url('/public/assets/js/jquery-3.7.1.min.js')}}"></script>
 
-		<!-- Datatable JS -->
-		<script src="{{url('/public/assets/plugins/datatables/datatables.min.js')}}"></script>
+    <!-- Bootstrap Core JS -->
+    <script src="{{url('/public/assets/js/bootstrap.bundle.min.js')}}"></script>
 
-		<!-- select CSS -->
-		<script src="{{url('/public/assets/plugins/select2/js/select2.min.js')}}"></script>
-		
-		<!-- Slimscroll JS -->
-		<script src="{{url('/public/assets/plugins/slimscroll/jquery.slimscroll.min.js')}}"></script>
-		
-		<!-- Datepicker Core JS -->
-		<script src="{{url('/public/assets/plugins/moment/moment.min.js')}}"></script>
-		<script src="{{url('/public/assets/js/bootstrap-datetimepicker.min.js')}}"></script>
-		
-		<!-- multiselect JS -->
-		<script src="{{url('/public/assets/js/jquery-ui.min.js')}}"></script>
+    <!-- Datatable JS -->
+    <script src="{{url('/public/assets/plugins/datatables/datatables.min.js')}}"></script>
 
-		<!-- Theme Settings JS -->
-		<script src="{{url('/public/assets/js/theme-settings.js')}}"></script>
-		<script src="{{url('/public/assets/js/greedynav.js')}}"></script>
-		
-		<!-- Custom JS -->
-		<script src="{{url('/public/assets/js/script.js')}}"></script>
-		<!-- Summernote JS -->
-		<script src="{{url('/public/assets/plugins/summernote/summernote-bs4.min.js')}}"></script>
+    <!-- select CSS -->
+    <script src="{{url('/public/assets/plugins/select2/js/select2.min.js')}}"></script>
 
-	</body>
+    <!-- Slimscroll JS -->
+    <script src="{{url('/public/assets/plugins/slimscroll/jquery.slimscroll.min.js')}}"></script>
+
+    <!-- Datepicker Core JS -->
+    <script src="{{url('/public/assets/plugins/moment/moment.min.js')}}"></script>
+    <script src="{{url('/public/assets/js/bootstrap-datetimepicker.min.js')}}"></script>
+
+    <!-- multiselect JS -->
+    <script src="{{url('/public/assets/js/jquery-ui.min.js')}}"></script>
+
+    <!-- Theme Settings JS -->
+    <script src="{{url('/public/assets/js/theme-settings.js')}}"></script>
+    <script src="{{url('/public/assets/js/greedynav.js')}}"></script>
+
+    <!-- Custom JS -->
+    <script src="{{url('/public/assets/js/script.js')}}"></script>
+    <!-- Summernote JS -->
+    <script src="{{url('/public/assets/plugins/summernote/summernote-bs4.min.js')}}"></script>
+    <script>
+    $(document).on('click', '#toggle_btn', function() {
+        //$('.sorting_desc').trigger('click');
+        setTimeout(function() {
+            $('.card-table .card-body .table th:first-child').trigger('click');
+        }, 300); // 1000 milliseconds = 1 second
+		setTimeout(function() {
+            $('.card-table .card-body .table th:first-child').trigger('click');
+        }, 300);
+    });
+    </script>
+</body>
 
 <!-- Mirrored from kanakku.dreamstechnologies.com/html/template/users.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 07 Feb 2024 11:49:49 GMT -->
+
 </html>
